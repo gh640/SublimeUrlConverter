084d604599035114e3b63364a921aa785ba2f6d7
Goto Hayato <habita.gh@gmail.com>
2017-07-06 22:28:59 +0900

    Replace threading to concurrent.futures for better performance.

--- UrlConverter.py
+++ UrlConverter.py
@@ -3,8 +3,8 @@
 """Converts selected URLs to links with fetched page titles.
 """
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
-from threading import Thread
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
@@ -13,83 +13,103 @@ import requests
 import sublime
 import sublime_plugin
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = "Goto Hayato"
 __copyright__ = 'Copyright 2017, Goto Hayato'
 __license__ = 'MIT'
 
 
-class TitleFetchThread(Thread):
-    """Thread for fetching a html page title.
+class TitleFetcher:
+    """Webpage title fetcher with multithreading.
     """
 
-    def __init__(self, url):
-        self.url = url
-        super().__init__()
+    def fetch(self, urls):
+        results = []
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = (executor.submit(self.fetch_title, url) for url in urls)
 
-    def run(self):
+            for f in as_completed(futures):
+                results.append(f.result())
+
+        return dict(results)
+
+    @staticmethod
+    def fetch_title(url):
         try:
-            response = requests.get(self.url)
+            response = requests.get(url)
             soup = BeautifulSoup(response.text, 'html.parser')
             title = soup.head.title.text
         except Exception as e:
             title = False
 
-        self.title = title
+        return (url, title)
 
 
-class BaseUrlConverter(sublime_plugin.TextCommand):
+class BaseUrlConverter:
     """Common abstract url converter.
     """
 
     REPL_TEMPLATE = ''
 
     def run(self, edit):
-        title_fetch_threads = self.fetch_title_with_treads()
-        region_and_repls = self.extract_links(title_fetch_threads)
+        region_and_urls = self.get_selected_urls()
+        urls = self.extract_unique_urls(region_and_urls)
+        url_titles_dict = self.fetch_titles(urls)
+        region_and_repls = self.combine_region_links(region_and_urls, url_titles_dict)
+
         self.replace_regions(edit, region_and_repls)
         sublime.status_message('UrlConverter: urls are converted successfully.')
 
-    def fetch_title_with_treads(self):
-        title_fetch_threads = []
-        for region in reversed(self.view.sel()):
+    def get_selected_urls(self):
+        region_and_urls = []
+        for region in self.view.sel():
             url = self.view.substr(region).strip()
             parsed = urlparse(url)
-            if parsed.scheme in ("http", "https"):
-                fetcher = TitleFetchThread(url)
-                fetcher.start()
-                title_fetch_threads.append((region, fetcher))
 
-        while True:
-            completed = True
-            for region, fetcher in title_fetch_threads:
-                if fetcher.is_alive():
-                    completed = False
-            if completed:
-                break
-            time.sleep(0.1)
+            if parsed.scheme not in ("http", "https"):
+                continue
 
-        return title_fetch_threads
+            region_and_urls.append((region, url))
 
-    def extract_links(self, title_fetch_threads):
-        return ((s, self.REPL_TEMPLATE.format(f)) if f.title else (s, f.url)
-                for s, f in title_fetch_threads)
+        return region_and_urls
+
+    def extract_unique_urls(self, region_and_urls):
+        return set(url for region, url in region_and_urls)
+
+    def fetch_titles(self, urls):
+        fetcher = TitleFetcher()
+        return fetcher.fetch(urls)
+
+        return fetcher.results
+
+    def combine_region_links(self, region_and_urls, url_titles_dict):
+        region_and_repls = []
+        for region, url in region_and_urls:
+            if url_titles_dict[url]:
+                repl = self.REPL_TEMPLATE.format(url=url, title=url_titles_dict[url])
+            else:
+                repl = False
+
+            region_and_repls.append((region, repl))
+
+        return region_and_repls
 
     def replace_regions(self, edit, region_and_repls):
-        for region, repl in region_and_repls:
+        # Replace regions from the last to avoid misselection.
+        for region, repl in sorted(region_and_repls, key=lambda x: x[0], reverse=True):
             if repl:
                 self.view.replace(edit, region, repl)
 
 
-class UrlConverterConvertToHtml(BaseUrlConverter):
-    """Html Url converter command.
+class UrlConverterConvertToHtml(BaseUrlConverter, sublime_plugin.TextCommand):
+    """Html url converter command.
     """
 
-    REPL_TEMPLATE = '<a href="{0.url}">{0.title}</a>'
+    REPL_TEMPLATE = '<a href="{url}">{title}</a>'
 
 
-class UrlConverterConvertToMarkdown(BaseUrlConverter):
-    """Html Url converter command.
+class UrlConverterConvertToMarkdown(BaseUrlConverter, sublime_plugin.TextCommand):
+    """Markdown url converter command.
     """
 
-    REPL_TEMPLATE = '[{0.title}]({0.url})'
+    REPL_TEMPLATE = '[{title}]({url})'
