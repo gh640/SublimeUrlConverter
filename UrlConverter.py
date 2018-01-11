34ba54372dadd094629c2c0a485b1315f1a3cde1
Goto Hayato <habita.gh@gmail.com>
2018-01-11 11:20:29 +0900

    Update the version to 0.3.x and update the year in copyright string.

--- UrlConverter.py
+++ UrlConverter.py
@@ -12,9 +12,9 @@ import requests
 import sublime
 import sublime_plugin
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = "Goto Hayato"
-__copyright__ = 'Copyright 2017, Goto Hayato'
+__copyright__ = 'Copyright 2018, Goto Hayato'
 __license__ = 'MIT'
 
 
