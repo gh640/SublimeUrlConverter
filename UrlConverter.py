047493bcf7edd33a11a67b4fa0e282c44ed3779d
Goto Hayato <habita.gh@gmail.com>
2018-06-25 13:58:09 +0900

    Issue #3: Update version to 0.3.1.

--- UrlConverter.py
+++ UrlConverter.py
@@ -12,7 +12,7 @@ import requests
 import sublime
 import sublime_plugin
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = "Goto Hayato"
 __copyright__ = 'Copyright 2018, Goto Hayato'
 __license__ = 'MIT'
